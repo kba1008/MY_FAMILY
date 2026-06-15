c0e4de0e5af2baea5304658b6ae782f51999a7ac
kba1008 <sekolah-4896@moe-dl.edu.my>
2026-06-16 06:20:11 +0800

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
  *  Backend: Google Apps Script Web App (Code.gs)
  *  ===================================================================== */
 
-const API_URL = 'https://script.google.com/macros/s/AKfycbxVGz059cuL-l7CEHABQ57UT46BMZz1CEy6tl27cCpXIMgwJlmmKVpWCj1124kQn9f15A/exec'; // <-- TAMPAL URL Web App Apps Script di sini
+const API_URL = 'https://script.google.com/macros/s/AKfycbz1nEls_MBS4B98BEDazTDmdryU0gM1XcTI0IQbnJl_oRUCDCNh3eJBs1A4mM6ZEQbovw/exec'; // <-- TAMPAL URL Web App Apps Script di sini
 
 const POLL_INTERVAL = 2500;
 // (Kata laluan admin disimpan di server sahaja — tidak didedahkan di UI)
